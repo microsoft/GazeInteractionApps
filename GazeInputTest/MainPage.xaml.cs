34c8540cbbd52ed55be62ad42bd5e66b02fe1d33
Harish S. Kulkarni <harishsk@microsoft.com>
2017-12-13 12:37:32 -0800

    Initial checkin of gaze interaction library.

---
+++ GazeInputTest/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Research.Input.Gaze;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace GazeInputTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        GazePointer _gazePointer;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            _gazePointer = new GazePointer(this);
+        }
+
+        private void Dwell_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Microsoft.Research.Input.Gaze/OneEuroFilter.h
@@ -0,0 +1,59 @@
+#pragma once
+#include "pch.h"
+#include "IGazeFilter.h"
+
+using namespace Windows::Foundation;
+
+BEGIN_NAMESPACE_GAZE_INPUT
+
+public ref class LowpassFilter sealed
+{
+public:
+    LowpassFilter()
+    {
+        Previous = Point(0, 0);
+    }
+
+    LowpassFilter(Point initial)
+    {
+        Previous = initial;
+    }
+
+    property Point Previous;
+
+    Point Update(Point point, Point alpha)
+    {
+        Point pt;
+        pt.X = (alpha.X * point.X) + ((1 - alpha.X) * Previous.X);
+        pt.Y = (alpha.Y * point.Y) + ((1 - alpha.Y) * Previous.Y);
+        Previous = pt;
+        return Previous;
+    }
+};
+
+const float EUROFILTER_DEFAULT_BETA = 5.0f;
+const float EUROFILTER_DEFAULT_CUTOFF = 0.1f;
+const float EUROFILTER_DEFAULT_VELOCITY_CUTOFF = 1.0f;
+
+public ref class OneEuroFilter sealed : public IGazeFilter
+{
+public:
+    OneEuroFilter();
+    OneEuroFilter(float cutoff, float beta);
+    virtual GazeEventArgs^ Update(GazeEventArgs^ args);
+
+public:
+    property float Beta;
+    property float Cutoff;
+    property float VelocityCutoff;
+
+private:
+    float  Alpha(float rate, float cutoff);
+
+private:
+    long long       _lastTimestamp;
+    LowpassFilter^  _pointFilter;
+    LowpassFilter^  _deltaFilter;
+};
+
+END_NAMESPACE_GAZE_INPUT
