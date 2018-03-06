fefc76aec7f18af77ca13853221b2762ad717530
Jon Campbell <joncamp@microsoft.com>
2018-03-06 10:35:53 -0800

    Add mini copyright headers to source files

--- ControlTest/MainPage.xaml.cs
+++ ControlTest/MainPage.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿//Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. 
+//See LICENSE in the project root for license information. 
+
+using System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 using Microsoft.Research.Input.Gaze;
 using Windows.UI.Popups;
 
