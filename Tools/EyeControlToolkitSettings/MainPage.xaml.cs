0841b617e2ec6b0b97ec0665a5ebc18687f3c76d
Jon Campbell <joncamp@microsoft.com>
2018-03-26 13:53:27 -0700

    Minor refactor to remove duplicated code

--- Tools/EyeControlToolkitSettings/MainPage.xaml.cs
+++ Tools/EyeControlToolkitSettings/MainPage.xaml.cs
@@ -19,21 +19,7 @@ namespace EyeControlToolkitSettings
 
             _gazePointer = new GazePointer(this);
             _gazePointer.OnGazePointerEvent += OnGazePointerEvent;
-
-            OneEuroFilter_Beta_Textbox.Text = App._gazeSettings.OneEuroFilter_Beta.ToString();
-            OneEuroFilter_Cutoff_Textbox.Text = App._gazeSettings.OneEuroFilter_Cutoff.ToString();
-            OneEuroFilter_Velocity_Cutoff_Textbox.Text = App._gazeSettings.OneEuroFilter_Velocity_Cutoff.ToString();
-
-            GazePointer_Fixation_Delay_Textbox.Text = App._gazeSettings.GazePointer_Fixation_Delay.ToString();
-            GazePointer_Dwell_Delay_Textbox.Text = App._gazeSettings.GazePointer_Dwell_Delay.ToString();
-            GazePointer_Repeat_Delay_Textbox.Text = App._gazeSettings.GazePointer_Repeat_Delay.ToString();
-            GazePointer_Enter_Exit_Delay_Textbox.Text = App._gazeSettings.GazePointer_Enter_Exit_Delay.ToString();
-            GazePointer_Max_History_Duration_Textbox.Text = App._gazeSettings.GazePointer_Max_History_Duration.ToString();
-            GazePointer_Max_Single_Sample_Duration_Textbox.Text = App._gazeSettings.GazePointer_Max_Single_Sample_Duration.ToString();
-            GazePointer_Gaze_Idle_Time_Textbox.Text = App._gazeSettings.GazePointer_Gaze_Idle_Time.ToString();
-
-            GazeCursor_Cursor_Radius_Textbox.Text = App._gazeSettings.GazeCursor_Cursor_Radius.ToString();
-            GazeCursor_Cursor_Visibility_Textbox.Text = App._gazeSettings.GazeCursor_Cursor_Visibility.ToString();
+            LoadSettingsToUserControls();
         }
 
         private void OnGazePointerEvent(GazePointer sender, GazePointerEventArgs ea)
@@ -48,6 +34,18 @@ namespace EyeControlToolkitSettings
         {
             GazeSettingsHelpers.GazeSettingsFromLocalSettings(App._gazeSettings);
 
+            LoadSettingsToUserControls();
+        }
+
+        private void SetButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            SaveSettingsFromUserControls();
+
+            GazeSettingsHelpers.LocalSettingsFromGazeSettings(App._gazeSettings);
+        }
+
+        private void LoadSettingsToUserControls()
+        {
             OneEuroFilter_Beta_Textbox.Text = App._gazeSettings.OneEuroFilter_Beta.ToString();
             OneEuroFilter_Cutoff_Textbox.Text = App._gazeSettings.OneEuroFilter_Cutoff.ToString();
             OneEuroFilter_Velocity_Cutoff_Textbox.Text = App._gazeSettings.OneEuroFilter_Velocity_Cutoff.ToString();
@@ -64,7 +62,7 @@ namespace EyeControlToolkitSettings
             GazeCursor_Cursor_Visibility_Textbox.Text = App._gazeSettings.GazeCursor_Cursor_Visibility.ToString();
         }
 
-        private void SetButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        private void SaveSettingsFromUserControls()
         {
             App._gazeSettings.OneEuroFilter_Beta = float.Parse(OneEuroFilter_Beta_Textbox.Text);
             App._gazeSettings.OneEuroFilter_Cutoff = float.Parse(OneEuroFilter_Cutoff_Textbox.Text);
@@ -80,8 +78,6 @@ namespace EyeControlToolkitSettings
 
             App._gazeSettings.GazeCursor_Cursor_Radius = int.Parse(GazeCursor_Cursor_Radius_Textbox.Text);
             App._gazeSettings.GazeCursor_Cursor_Visibility = bool.Parse(GazeCursor_Cursor_Visibility_Textbox.Text);
-
-            GazeSettingsHelpers.LocalSettingsFromGazeSettings(App._gazeSettings);
         }
     }
 }
