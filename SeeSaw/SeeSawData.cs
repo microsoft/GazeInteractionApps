2f4a78941d1e340b8285d0921b32516fe1ddeada
Pete Ansell <petea@microsoft.com>
2018-03-29 10:48:47 -0700

    Added SeeSaw back

---
+++ SeeSaw/SeeSawData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Research.Input.Gaze;
+namespace SeeSaw
+{
+    public class SeeSawData
+    {
+        private static SeeSawData _instance;
+
+        public Dictionary<string, List<GazeEventArgs>> GazeData;
+
+        public SeeSawData()
+        {
+            GazeData = new Dictionary<string, List<GazeEventArgs>>();
+        }
+
+        public static SeeSawData Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                     _instance = new SeeSawData();
+                }
+                return _instance;
+            }
+        }
+    };
+}
