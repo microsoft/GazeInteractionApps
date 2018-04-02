534f2cb0eb09aa9b769d92d31f4baa84b0720a0b
Jon Campbell <joncamp@microsoft.com>
2018-04-02 16:14:27 -0700

    Remove lesser used settings

--- Tools/EyeControlToolkitSettings/MainPage.xaml.cs
+++ Tools/EyeControlToolkitSettings/MainPage.xaml.cs
@@ -39,36 +39,6 @@ namespace EyeControlToolkitSettings
         }
 
         #region Button Handlers
-        private void OneEuroFilterBeta_NudgeDown_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
-            GazeSettings.OneEuroFilterBeta -= 1f;
-        }
-
-        private void OneEuroFilterBeta_NudgeUp_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
-            GazeSettings.OneEuroFilterBeta += 1f;
-        }
-
-        private void OneEuroFilterCutoff_NudgeDown_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
-            GazeSettings.OneEuroFilterCutoff -= 0.01f;
-        }
-
-        private void OneEuroFilterCutoff_NudgeUp_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
-            GazeSettings.OneEuroFilterCutoff += 0.01f;
-        }
-
-        private void OneEuroFilterVelocityCutoff_NudgeDown_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
-            GazeSettings.OneEuroFilterVelocityCutoff -= 0.1f;
-        }
-
-        private void OneEuroFilterVelocityCutoff_NudgeUp_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
-            GazeSettings.OneEuroFilterVelocityCutoff += 0.1f;
-        }
-
         private void GazePointerFixationDelay_NudgeDown_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             GazeSettings.GazePointerFixationDelay -= 50000;
@@ -107,26 +77,6 @@ namespace EyeControlToolkitSettings
         private void GazePointerEnterExitDelay_NudgeUp_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             GazeSettings.GazePointerEnterExitDelay += 50000;
-        }
-
-        private void GazePointerMaxHistoryDuration_NudgeDown_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
-
-        }
-
-        private void GazePointerMaxHistoryDuration_NudgeUp_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
-
-        }
-
-        private void GazePointerMaxSingleSampleDuration_NudgeDown_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
-
-        }
-
-        private void GazePointerMaxSingleSampleDuration_NudgeUp_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
-
         }
 
         private void GazePointerGazeIdleTime_NudgeDown_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
