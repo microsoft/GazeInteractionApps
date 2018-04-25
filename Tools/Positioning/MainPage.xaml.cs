93261cde08ccd9d373601af055f2b3c9aa96b130
Jon Campbell <joncamp@microsoft.com>
2018-04-25 13:30:14 -0700

    Initial positioning app

---
+++ Tools/Positioning/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Numerics;
+using Windows.Devices.HumanInterfaceDevice;
+using Windows.Devices.Input.Preview;
+using Windows.UI.Xaml.Controls;
+using static Positioning.GazeHidInputReportHelpers;
+
+namespace Positioning
+{
+    public sealed partial class MainPage : Page
+    {
+        private GazeInputSourcePreview gazeInputSourcePreview;
+        const int GazeHidPage = 0x12;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            gazeInputSourcePreview = GazeInputSourcePreview.GetForCurrentView();
+            gazeInputSourcePreview.GazeMoved += GazeInputSourcePreview_GazeMoved;
+        }
+
+        private void GazeInputSourcePreview_GazeMoved(GazeInputSourcePreview sender, GazeMovedPreviewEventArgs args)
+        {
+            if (args.CurrentPoint.HidInputReport != null)
+            {
+                var hidReport = args.CurrentPoint.HidInputReport;
+                var sourceDevice = args.CurrentPoint.SourceDevice;
+
+                var leftEyePositionParser = new GazePositionHidParser(sourceDevice, GazeExtendedUsages.Usage_LeftEyePosition);
+                var rightEyePositionParser = new GazePositionHidParser(sourceDevice, GazeExtendedUsages.Usage_RightEyePosition);
+                var headPositionParser = new GazePositionHidParser(sourceDevice, GazeExtendedUsages.Usage_HeadPosition);
+
+                var leftEyePosition = leftEyePositionParser.GetPosition(hidReport);
+                var rightEyePosition = rightEyePositionParser.GetPosition(hidReport);
+                var headPosition = headPositionParser.GetPosition(hidReport);
+
+                Canvas.SetLeft(HeadPositionEllipse, headPosition.Value.X);
+                Canvas.SetTop(HeadPositionEllipse, headPosition.Value.Y);
+            }
+        }
+    }
+}
