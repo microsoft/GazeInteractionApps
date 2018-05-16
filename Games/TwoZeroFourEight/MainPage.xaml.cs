6475fd2490e4e92d80a8ca309e09725a3f3b86c5
Harish Kulkarni <harishsk@gmail.com>
2018-05-15 21:12:17 -0700

    Fixed 2048 perf issues and fixed several bugs. Updated other projects to latest version of .NetCore

--- Games/TwoZeroFourEight/MainPage.xaml.cs
+++ Games/TwoZeroFourEight/MainPage.xaml.cs
@@ -9,6 +9,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.System;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -21,13 +22,91 @@ using Windows.UI.Xaml.Navigation;
 
 namespace TwoZeroFourEight
 {
-    public class Cell
+    public class NotificationBase : INotifyPropertyChanged
     {
-        public string Value
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        protected virtual void OnPropertyChanged(string propertyName)
         {
-            get { return (IntVal != 0) ? IntVal.ToString() : ""; }
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
         }
-        public int IntVal;
+
+        protected bool SetField<T>(ref T field, T value, string propertyName)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+
+    public sealed class ValueToBackgroundColorConverter : IValueConverter
+    {
+        private static Brush[] _backgroundColors = new Brush[]
+        {
+            new SolidColorBrush(Colors.LightGray),
+
+            new SolidColorBrush(Colors.PaleTurquoise),
+            new SolidColorBrush(Colors.LightBlue),
+            new SolidColorBrush(Colors.LightSkyBlue),
+            new SolidColorBrush(Colors.DeepSkyBlue),
+
+            new SolidColorBrush(Colors.LemonChiffon),
+            new SolidColorBrush(Colors.Khaki),
+            new SolidColorBrush(Colors.DarkKhaki),
+            new SolidColorBrush(Colors.Goldenrod),
+
+            new SolidColorBrush(Colors.PaleGreen),
+            new SolidColorBrush(Colors.LightGreen),
+            new SolidColorBrush(Colors.YellowGreen),
+            new SolidColorBrush(Colors.LimeGreen),
+
+            new SolidColorBrush(Colors.MistyRose),
+            new SolidColorBrush(Colors.Pink),
+            new SolidColorBrush(Colors.HotPink),
+            new SolidColorBrush(Colors.DeepPink),
+        };
+
+        public object Convert(object value, Type targetType,
+                              object parameter, string culture)
+        {
+            int index = 0;
+            int val = (int)value;
+            while (val > 0)
+            {
+                val >>= 1;
+                index++;
+            }
+            return _backgroundColors[index];
+        }
+
+        public object ConvertBack(object value, Type targetType,
+                                  object parameter, string culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Cell: NotificationBase
+    {
+        private int _intVal;
+        public int IntVal
+        {
+            get { return _intVal; }
+            set
+            {
+                SetField<int>(ref _intVal, value, "IntVal");
+            }
+        }
+
+        private Brush _backgroundColor;
+        public Brush BackgroundColor
+        {
+            get { return _backgroundColor; }
+            set { SetField<Brush>(ref _backgroundColor, value, "BackgroundColor"); }
+        }
+
     }
 
     public sealed class IntegerToStringConverter: IValueConverter
@@ -46,85 +125,146 @@ namespace TwoZeroFourEight
         }
     }
 
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed class BoolToVisibilityConverter : IValueConverter
     {
-        public const int BOARD_SIZE = 4;
-        public const int MAX_BOARD_CELLS = BOARD_SIZE * BOARD_SIZE;
-        private ObservableCollection<int> _board;
-        private Random _random = new Random();
-
-        public int Score { get; set; }
-        public Visibility GameOverStatus { get; set; }
-
-        public MainPage()
+        public object Convert(object value, Type targetType,
+                              object parameter, string culture)
         {
-            this.InitializeComponent();
-
-            _board = new ObservableCollection<int>();
-            for (int i = 0; i < MAX_BOARD_CELLS; i++)
-            {
-                //var cell = new Cell { IntVal = 0 };
-                _board.Add(0);
-            }
-
-            InitializeGame();
+            bool val = (bool)value;
+            return val ? Visibility.Visible : Visibility.Collapsed;
         }
 
-        private void InitializeGame()
+        public object ConvertBack(object value, Type targetType,
+                                  object parameter, string culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Board : NotificationBase
+    {
+        private int _boardSize;
+        private int _maxCells;
+        private Random _random = new Random();
+
+        private bool _gameOver;
+        public bool GameOver
+        {
+            get { return _gameOver; }
+            set { SetField<bool>(ref _gameOver, value, "GameOver"); }
+        }
+
+        private int _score;
+        public int Score
+        {
+            get { return _score; }
+            set { SetField<int>(ref _score, value, "Score"); }
+        }
+
+        public List<Cell> Cells { get; set; }
+
+        public Board(int boardSize)
+        {
+            _boardSize = boardSize;
+            _maxCells = _boardSize * _boardSize;
+
+            Cells = new List<Cell>(_maxCells);
+            for (int i = 0; i < _maxCells; i++)
+            {
+                Cells.Add(new Cell());
+            }
+
+            Reset();
+        }
+
+        public void Reset()
         {
             Score = 0;
-            for (int i = 0; i < MAX_BOARD_CELLS; i++)
+            GameOver = false;
+            for (int i = 0; i < _maxCells; i++)
             {
-                //var cell = new Cell { IntVal = 0 };
-                _board[i] = 0;
+                Cells[i].IntVal = 0;
             }
-            GameOverStatus = Visibility.Collapsed;
             GenerateNextTile();
         }
 
         private bool IsBoardFull()
         {
-            foreach (var cell in _board)
+            for (int i = 0; i < _boardSize; i++)
             {
-                if (cell == 0)
+                for (int j = 0; j < _boardSize; j++)
                 {
-                    return false;
+                    var cur = Cells[i * _boardSize + j];
+                    var above = (i > 0) ? Cells[((i - 1) * _boardSize) + j] : null;
+                    var below = (i < _boardSize - 1) ? Cells[((i + 1) * _boardSize) + j] : null;
+                    var left = (j > 0) ? Cells[(i * _boardSize) + j - 1] : null;
+                    var right = (j < _boardSize - 1) ? Cells[(i * _boardSize) + j + 1] : null;
+
+                    if ((above != null) && (cur.IntVal == above.IntVal))
+                    {
+                        return false;
+                    }
+
+                    if ((below != null) && (cur.IntVal == below.IntVal))
+                    {
+                        return false;
+                    }
+
+                    if ((left != null) && (cur.IntVal == left.IntVal))
+                    {
+                        return false;
+                    }
+
+                    if ((right != null) && (cur.IntVal == right.IntVal))
+                    {
+                        return false;
+                    }
                 }
             }
             return true;
         }
 
-        private void GenerateNextTile()
+        public bool GenerateNextTile()
         {
-            int index;
-            do
+            List<Cell> blankCells = new List<Cell>();
+            for (int i = 0; i < _maxCells; i++)
             {
-                index = _random.Next(MAX_BOARD_CELLS);
-            } while (_board[index] != 0);
+                if (Cells[i].IntVal == 0)
+                {
+                    blankCells.Add(Cells[i]);
+                }
+            }
 
-            int val = (_random.Next(2) * 2) + 2;
+            if (blankCells.Count == 0)
+            {
+                return false;
+            }
 
-            _board[index] = val;
+            int index = _random.Next(blankCells.Count);
+
+            // generate a 2 mostly, but generate a 4 about one in 8 times
+            int val = (_random.Next(8) > 0) ? 2 : 4;
+
+            blankCells[index].IntVal = val;
+
+            return true;
         }
 
         private bool SlideRowOrCol(int start, int end, int delta)
         {
             bool slideSuccess = false;
             int cur = start;
-            for (int i = 0; i < BOARD_SIZE; i++)
+            for (int i = 0; i < _boardSize; i++)
             {
                 int j = cur;
-                while ((j != end) && (_board[j] == 0))
+                while ((j != end) && (Cells[j].IntVal == 0))
                 {
                     j += delta;
                 }
-                if ((j != cur) && (_board[j] != 0))
+                if ((j != cur) && (Cells[j].IntVal != 0))
                 {
-                    _board[cur] = _board[j];
-                    _board[j] = 0;
+                    Cells[cur].IntVal = Cells[j].IntVal;
+                    Cells[j].IntVal = 0;
                     slideSuccess = true;
                 }
                 cur += delta;
@@ -136,13 +276,13 @@ namespace TwoZeroFourEight
         {
             bool addSuccess = false;
             int cur = start;
-            for (int i = 0; i < BOARD_SIZE - 1; i++)
+            for (int i = 0; i < _boardSize - 1; i++)
             {
-                if ((_board[cur + delta] != 0) && (_board[cur] > 0) && (_board[cur] == _board[cur + delta]))
+                if ((Cells[cur + delta].IntVal != 0) && (Cells[cur].IntVal > 0) && (Cells[cur].IntVal == Cells[cur + delta].IntVal))
                 {
-                    _board[cur] += _board[cur + delta];
-                    _board[cur + delta] = 0;
-                    Score += _board[cur];
+                    Cells[cur].IntVal += Cells[cur + delta].IntVal;
+                    Cells[cur + delta].IntVal = 0;
+                    Score += Cells[cur].IntVal;
                     addSuccess = true;
                 }
                 cur += delta;
@@ -152,8 +292,13 @@ namespace TwoZeroFourEight
 
         private void SlideBoard(int start, int end, int delta, int increment)
         {
+            if (GameOver)
+            {
+                return;
+            }
+
             bool change = false;
-            for (int i = 0; i < BOARD_SIZE; i++)
+            for (int i = 0; i < _boardSize; i++)
             {
                 change = SlideRowOrCol(start, end, delta) || change;
                 change = AddAdjacent(start, end, delta) || change;
@@ -162,41 +307,77 @@ namespace TwoZeroFourEight
                 end += increment;
             }
 
-            if (IsBoardFull())
-            {
-                GameOverStatus = Visibility.Visible;
-                return;
-            }
-
             if (change)
             {
-                GenerateNextTile();
+                OnPropertyChanged("Score");
             }
+
+            if ((!GenerateNextTile()) && IsBoardFull())
+            {
+                GameOver = true;
+                OnPropertyChanged("GameOver");
+            }
+
+        }
+
+        public void SlideLeft()
+        {
+            SlideBoard(0, _boardSize - 1, 1, _boardSize);
+        }
+
+        public void SlideRight()
+        {
+            SlideBoard(_boardSize - 1, 0, -1, _boardSize);
+        }
+
+        public void SlideUp()
+        {
+            SlideBoard(0, _maxCells - _boardSize, _boardSize, 1);
+        }
+
+        public void SlideDown()
+        {
+            SlideBoard(_maxCells - _boardSize, 0, -_boardSize, 1);
+        }
+    }
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public Board Board;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Board = new Board(4);
+            DataContext = Board;
         }
 
         private void OnNewGame(object sender, RoutedEventArgs e)
         {
-            InitializeGame();
+            Board.Reset();
         }
 
         private void OnUpClick(object sender, RoutedEventArgs e)
         {
-            SlideBoard(0, MAX_BOARD_CELLS - BOARD_SIZE, BOARD_SIZE, 1);
+            Board.SlideUp();
         }
 
         private void OnLeftClick(object sender, RoutedEventArgs e)
         {
-            SlideBoard(0, BOARD_SIZE - 1, 1, BOARD_SIZE);
+            Board.SlideLeft();
         }
 
         private void OnRightClick(object sender, RoutedEventArgs e)
         {
-            SlideBoard(BOARD_SIZE - 1, 0, -1, BOARD_SIZE);
+            Board.SlideRight();
         }
 
         private void OnDownClick(object sender, RoutedEventArgs e)
         {
-            SlideBoard(MAX_BOARD_CELLS - BOARD_SIZE, 0, -BOARD_SIZE, 1);
+            Board.SlideDown();
         }
 
         private void OnPageKeyUp(object sender, KeyRoutedEventArgs e)
@@ -204,16 +385,16 @@ namespace TwoZeroFourEight
             switch (e.Key)
             {
                 case VirtualKey.Up:
-                    OnUpClick(null, null);
+                    Board.SlideUp();
                     break;
                 case VirtualKey.Down:
-                    OnDownClick(null, null);
+                    Board.SlideDown();
                     break;
                 case VirtualKey.Left:
-                    OnLeftClick(null, null);
+                    Board.SlideLeft();
                     break;
                 case VirtualKey.Right:
-                    OnRightClick(null, null);
+                    Board.SlideRight();
                     break;
             }
         }
