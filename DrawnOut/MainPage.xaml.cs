500db09f3baab5abdfe5ecbc4fa8ec194afa4094
Jon Campbell <joncamp@microsoft.com>
2018-05-23 14:08:22 -0700

    Initial Implementation of DrawnOut
    This app provides a blank canvas for creating eye controlled drawings.

---
+++ DrawnOut/MainPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.ObjectModel;
+using Windows.Devices.Input.Preview;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DrawnOut
+{
+    public class PointPair
+    {
+        public Point P1 { get; set; }
+
+        public Point P2 { get; set; }
+
+        public PointPair(Point p1, Point p2)
+        {
+            P1 = p1;
+            P2 = p2;
+        }
+    }
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private GazeInputSourcePreview gazeInputSourcePreview;
+        private Frame rootFrame;
+        private Point? oldPoint = null;
+
+        public ObservableCollection<PointPair> GazeHistory { get; set; } = new ObservableCollection<PointPair>();
+
+        public int MaxGazeHistorySize { get; set; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            MaxGazeHistorySize = 100000;
+
+            rootFrame = Window.Current.Content as Frame;
+
+            gazeInputSourcePreview = GazeInputSourcePreview.GetForCurrentView();
+            gazeInputSourcePreview.GazeMoved += GazeInputSourcePreview_GazeMoved;
+
+            GazeHistoryItemsControl.ItemsSource = GazeHistory;
+        }
+        
+        private void GazeInputSourcePreview_GazeMoved(GazeInputSourcePreview sender, GazeMovedPreviewEventArgs args)
+        {
+            UpdateGazeHistory(args.CurrentPoint);
+        }
+
+        private void UpdateGazeHistory(GazePointPreview pt)
+        {
+            if (pt.EyeGazePosition.HasValue)
+            {
+                var transform = rootFrame.TransformToVisual(this);
+                var newPoint = transform.TransformPoint(pt.EyeGazePosition.Value);
+
+                if (oldPoint.HasValue)
+                {
+                    GazeHistory.Add(new PointPair(oldPoint.Value, newPoint));
+
+                    if (GazeHistory.Count > MaxGazeHistorySize)
+                    {
+                        GazeHistory.RemoveAt(0);
+                    }
+                }
+
+                oldPoint = newPoint;
+            }
+        }
+
+        private void Page_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Escape)
+            {
+                App.Current.Exit();
+            }
+            else if (e.Key == Windows.System.VirtualKey.Delete ||
+                e.Key == Windows.System.VirtualKey.Back)
+            {
+                GazeHistory.Clear();
+            }
+        }
+    }
+}
