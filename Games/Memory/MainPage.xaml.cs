61aba807d8ae17dc0462d170e460c2fba95fc8bc
Jon Campbell <joncamp@microsoft.com>
2018-06-19 14:36:09 -0700

    Minor cleanup

--- Games/Memory/MainPage.xaml.cs
+++ Games/Memory/MainPage.xaml.cs
@@ -4,8 +4,6 @@ using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace Memory
 {
     /// <summary>
