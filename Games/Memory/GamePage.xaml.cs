8b72956ca93b66b9fb7d8f54652f4cb3c5eb1c51
VisualByteStudios <dwayne@visualbytestudios.com>
2019-07-15 11:52:56 -0700

    Accessibility fix for focus on page load

--- Games/Memory/GamePage.xaml.cs
+++ Games/Memory/GamePage.xaml.cs
@@ -110,8 +110,21 @@ namespace Memory
             }
         }
 
+        private ScrollViewer getRootScrollViewer()
+        {
+            DependencyObject el = this;
+            while (el != null && !(el is ScrollViewer))
+            {
+                el = VisualTreeHelper.GetParent(el);
+            }
+
+            return (ScrollViewer)el;
+        }
+
         private void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
+            getRootScrollViewer().Focus(FocusState.Programmatic);
+
             ArrangeBoardLayout();
             ResetBoard();
         }
